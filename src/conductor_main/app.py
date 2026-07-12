b579f086e8a996caf613df266a1851a8136d7452
Patrick Lacey <pat@laceys.com>
2026-07-12 01:19:06 -0400

    chore(license): add LGPL metadata and source notices

--- src/conductor_main/app.py
+++ src/conductor_main/app.py
@@ -8,6 +8,21 @@ Features:
 - Toggleable history sidebar panel
 """
 
+# Copyright (C) 2026 Conductor contributors
+#
+# Conductor Main is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Conductor Main is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Conductor Main. If not, see <https://www.gnu.org/licenses/>.
+
 import logging
 import os
 from concurrent.futures import ThreadPoolExecutor
--- src/conductor_main/visualization.py
+++ src/conductor_main/visualization.py
@@ -1,5 +1,20 @@
 """Plotly piano-roll visualization owned by the Gradio client."""
 
+# Copyright (C) 2026 Conductor contributors
+#
+# Conductor Main is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Conductor Main is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Conductor Main. If not, see <https://www.gnu.org/licenses/>.
+
 import mido
 import plotly.graph_objects as go
 
